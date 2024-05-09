f4a03a3de981cc1850381d8188eb352e9f5032ec
upuphero <540864988@qq.com>
2024-05-08 21:42:23 -0700

    Add docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:true,
-    port:3000,
-   
-  }
-})
+  server: {
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+});
